ce11d157637f09cef98a0b31862d87b94b68998a
Bryanw <jiayu1.wu@intel.com>
2024-04-22 17:45:14 -0300

    Update client.go

--- pkg/client/live/client.go
+++ pkg/client/live/client.go
@@ -18,10 +18,10 @@ import (
 	"github.com/dvonthenen/websocket"
 	klog "k8s.io/klog/v2"
 
-	live "github.com/deepgram/deepgram-go-sdk/pkg/api/live/v1"
-	msginterfaces "github.com/deepgram/deepgram-go-sdk/pkg/api/live/v1/interfaces"
-	version "github.com/deepgram/deepgram-go-sdk/pkg/api/version"
-	interfaces "github.com/deepgram/deepgram-go-sdk/pkg/client/interfaces"
+	live "github.com/bryanwux/deepgram-go-sdk/pkg/api/live/v1"
+	msginterfaces "github.com/bryanwux/deepgram-go-sdk/pkg/api/live/v1/interfaces"
+	version "github.com/bryanwux/deepgram-go-sdk/pkg/api/version"
+	interfaces "github.com/bryanwux/deepgram-go-sdk/pkg/client/interfaces"
 )
 
 /*
